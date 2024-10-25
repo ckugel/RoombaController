79d953e370703b890f5e1759296624b2ae91b1fb
ckugel <calebkugel1@gmail.com>
2024-10-25 13:12:04 -0500

    started pillar

--- src/Pillar.cpp
+++ src/Pillar.cpp
@@ -2,4 +2,15 @@
 // Created By Caleb on 10/24/2024
 //
 
+#include "Pillar.h"
+
+Pillar::Pillar(Pose2D position, double radius) {
+  this->position = position;
+  this->radius = radius;
+}
+
+Pillar::Pillar(double x, double y, double radius) {
+  this->position = pose(x, y);
+  this->radius = radius;
+}
 
