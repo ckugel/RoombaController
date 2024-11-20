6bbd34fb73743943900f9dd9ddbdfcfdc873d26e
Caleb Kugel <calebkugel1@gmail.com>
2024-11-19 22:51:39 -0600

    WIP

--- src/SlopeIntercept.cpp
+++ src/SlopeIntercept.cpp
@@ -3,11 +3,18 @@
  * @Author ckugel
  */
 
+#include "SlopeIntercept.hpp"
+
 SlopeIntercept::SlopeIntercept() {
 
 }
 
+SlopeIntercept::SlopeIntercept(Pose2D one, Pose2D two) {
+    ()
+}
 
+SlopeIntercept::SlopeIntercept(double X1, double Y1, double X2, double Y2) {
 
+}
 
 
